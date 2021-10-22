79a911a25a7828f1c008405712722ddded1ae5e5
Fabian Harmsen <git@lfs96.de>
2021-10-22 23:13:48 +0200

    Result printing improved

--- downloadAdvertisments.py
+++ downloadAdvertisments.py
@@ -9,13 +9,13 @@ def update_advertisements(aid, data):
     my_cursor = mydb.cursor()
 
     sql = "UPDATE mobilede.advertisements SET raw = %s, status = %s WHERE id = %s"
-    val = (data,1,  aid)
+    val = (data, 1,  aid)
 
     my_cursor.execute(sql, val)
     mydb.commit()
 
 def downloadUrl(url):
-    print(url)
+    #print(url)
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}
     html_content = requests.get(url, headers=header)
     return html_content.text
@@ -27,13 +27,14 @@ def get_next_advertisements(numbers):
     mycursor.execute("SELECT * FROM mobilede.advertisements WHERE status = 0 LIMIT "+str(numbers))
 
     myresult = mycursor.fetchall()
-
+    i = 0
     for x in myresult:
+        i += 1
         aid = x[0]
         url = x[1]
-        print(url)
+        print(str(i) + " - " + url)
         html = downloadUrl(url)
-        update_advertisements(aid,html)
+        update_advertisements(aid, html)
 
 if __name__ == '__main__':
-    get_next_advertisements(5000)
+    get_next_advertisements(120000)
