5852c5473c930516b580375dc18be413fb2aab89
Fabian Harmsen <git@lfs96.de>
2021-10-22 16:28:49 +0200

    Cleaned Input prepare for db import

--- anal.py
+++ anal.py
@@ -4,6 +4,8 @@ import mysql.connector
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
 from findNewAdvertisments import get_mysql_connector
+import datetime
+import re
 
 def get_data(num):
     mydb = get_mysql_connector()
@@ -13,33 +15,53 @@ def get_data(num):
     for x in myresult:
         return x[0]
 
-
 def anal_html(num):
     html = get_data(num)
     soup = BeautifulSoup(html, 'html.parser')
 
     print(soup.find('title').get_text())                                                    # Title
-    print(soup.find("span", {"class": "h3", "data-testid": "prime-price"}).get_text())      # Preis
-    print(soup.find("div", {"class": "g-col-6", "id": "damageCondition-v"}).get_text())     # Fahrzeugzustand
-    print(soup.find("div", {"class": "g-col-6", "id": "mileage-v"}).get_text())             # Laufleistung
-    print(soup.find("div", {"class": "g-col-6", "id": "cubicCapacity-v"}).get_text())       # Hubraum
-    print(soup.find("div", {"class": "g-col-6", "id": "power-v"}).get_text())               # Leistung
-    print(soup.find("div", {"class": "g-col-6", "id": "fuel-v"}).get_text())                # Treibstoff Art
-    print(soup.find("div", {"class": "g-col-6", "id": "envkv.emission-v"}).get_text())      # CO2
-    print(soup.find("div", {"class": "g-col-6", "id": "transmission-v"}).get_text())        # Schaltung
-    print(soup.find("div", {"class": "g-col-6", "id": "firstRegistration-v"}).get_text())   # Erstzulassung
 
-    verbrauch_html = soup.find("div", {"class": "g-col-6", "id": "envkv.consumption-v"})
-    verbrauch_soup = BeautifulSoup(str(verbrauch_html), 'html.parser')
-    for v in verbrauch_soup.find_all("div", {"class": "u-margin-bottom-9"}):
-        print(v.get_text())                                                                 # Verbrauch
+    preis = int("".join(filter(str.isdigit, soup.find("span", {"class": "h3", "data-testid": "prime-price"}).get_text())))
+    fahrzeugzustand = soup.find("div", {"class": "g-col-6", "id": "damageCondition-v"}).get_text()
+    laufleistung = int("".join(filter(str.isdigit, soup.find("div", {"class": "g-col-6", "id": "mileage-v"}).get_text())))
+    hubraum = int("".join(filter(str.isdigit, soup.find("div", {"class": "g-col-6", "id": "cubicCapacity-v"}).get_text()))[:-1])
+    leistung = int(soup.find("div", {"class": "g-col-6", "id": "power-v"}).get_text().split()[0])
+    treibstoff = soup.find("div", {"class": "g-col-6", "id": "fuel-v"}).get_text()
+    CO2 = int("".join(filter(str.isdigit, soup.find("div", {"class": "g-col-6", "id": "envkv.emission-v"}).get_text())))
+    schaltung = soup.find("div", {"class": "g-col-6", "id": "transmission-v"}).get_text()
+    erstzulassung = datetime.datetime.strptime(soup.find("div", {"class": "g-col-6", "id": "firstRegistration-v"}).get_text(), "%m/%Y").date()
+    description = soup.find("div", {"class": "description"}).get_text()
 
-    feature_html = soup.find("div", {"class": "cBox-body", "id": "features"})
-    feature_soup = BeautifulSoup(str(feature_html), 'html.parser')
-    for f in feature_soup.find_all("div", {"class": "bullet-list"}):
-        print(f.get_text())                                                                 # Features
+    i = 0
+    verbrauch_kombiniert = verbrauch_innerorts = verbrauch_ausserorts = 0
+    for v in BeautifulSoup(str(soup.find("div", {"class": "g-col-6", "id": "envkv.consumption-v"})), 'html.parser').find_all("div", {"class": "u-margin-bottom-9"}):
+        if i == 0:
+            verbrauch_kombiniert = float(v.get_text()[4:-20].replace(",", "."))
+        if i == 1:
+            verbrauch_innerorts = float(v.get_text()[4:-19].replace(",", "."))
+        if i == 2:
+            verbrauch_ausserorts = float(v.get_text()[4:-19].replace(",", "."))
+        i += 1
 
-    print(soup.find("div", {"class": "description"}).get_text())                            # Beschreibung laut Hersteller
+    features = []
+    for f in BeautifulSoup(str(soup.find("div", {"class": "cBox-body", "id": "features"})), 'html.parser').find_all(
+            "div", {"class": "bullet-list"}):
+        features.append(f.get_text())
+
+    print(preis)
+    print(fahrzeugzustand)
+    print(laufleistung)
+    print(hubraum)
+    print(leistung)
+    print(verbrauch_kombiniert)
+    print(verbrauch_innerorts)
+    print(verbrauch_ausserorts)
+    print(treibstoff)
+    print(CO2)
+    print(schaltung)
+    print(erstzulassung)
+    print(features)
+    print(description)
 
 
 
